181c897e8eacbda721f56133a9a756edc831e7e9
Jonathan Azoff <jon@azoffdesign.com>
2016-05-10 11:40:22 -0700

    cover plugins folder too

--- setup_craft.sh
+++ setup_craft.sh
@@ -3,6 +3,7 @@
 echo "=> Recursively owning craft folders..."
 chown -R www-data /var/www/craft/config
 chown -R www-data /var/www/craft/storage
+chown -R www-data /var/www/craft/plugins
 
 echo "=> Creating craft database..."
 echo 'CREATE DATABASE craft DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci' | mysql -u root
