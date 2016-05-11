ab43adc1435c71d01dbf39caa62d4d959b18f348
Jonathan Azoff <jon@azoffdesign.com>
2016-05-11 00:00:49 -0700

    simplify chown

--- setup_craft.sh
+++ setup_craft.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 echo "=> Recursively owning craft folders..."
-chown -R www-data /var/www/craft/config
-chown -R www-data /var/www/craft/storage
-chown -R www-data /var/www/craft/plugins
+chown -R www-data /var/www/craft
 
 echo "=> Creating craft database..."
 echo "CREATE DATABASE craft DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci" | mysql -u root
