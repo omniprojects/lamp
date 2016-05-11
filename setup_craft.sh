3b76cb392c0699cf41bbf3877d9f0a9c23335939
Jonathan Azoff <jon@azoffdesign.com>
2016-05-11 10:41:14 -0700

    actually import bootstrap

--- setup_craft.sh
+++ setup_craft.sh
@@ -8,4 +8,4 @@ echo "=> Creating craft database..."
 echo "CREATE DATABASE craft DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci" | mysql -u root
 
 echo "=> Bootstrapping craft database..."
-echo mysql -u root craft < /var/www/craft/config/bootstrap.sql
+mysql -u root craft < /var/www/craft/config/bootstrap.sql
