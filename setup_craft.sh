3e790d0b73aa8e1788aafefffb4f837f70098755
Jonathan Azoff <jon@azoffdesign.com>
2016-05-10 18:05:15 -0700

    add the assets folder

--- setup_craft.sh
+++ setup_craft.sh
@@ -4,6 +4,7 @@ echo "=> Recursively owning craft folders..."
 chown -R www-data /var/www/craft/config
 chown -R www-data /var/www/craft/storage
 chown -R www-data /var/www/craft/plugins
+chown -R www-data /var/www/assets
 
 echo "=> Creating craft database..."
 echo 'CREATE DATABASE craft DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci' | mysql -u root
