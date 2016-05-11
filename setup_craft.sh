36cb02036222b446395cbede6c88b1d682d510ca
Jonathan Azoff <jon@azoffdesign.com>
2016-05-11 00:26:46 -0700

    chown is back

--- setup_craft.sh
+++ setup_craft.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "=> Updating folder ownership..."
+chown -R www-data /var/www/craft/storage
+chown -R www-data /var/www/craft/plugins
+
 echo "=> Creating craft database..."
 echo "CREATE DATABASE craft DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci" | mysql -u root
 
